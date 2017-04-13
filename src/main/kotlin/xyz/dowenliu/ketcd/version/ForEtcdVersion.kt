12269a9c2e46ebf1b90a532547acc347663b911a
dowenliu-xyz <hawkdowen@hotmail.com>
2017-04-13 21:07:55 +0800

    add: annotation ForEtcdVersion
    Annotate the etcd version that the function, field or type works with.

---
+++ src/main/kotlin/xyz/dowenliu/ketcd/version/ForEtcdVersion.kt
@@ -0,0 +1,16 @@
+package xyz.dowenliu.ketcd.version
+
+import kotlin.annotation.AnnotationRetention.*
+import kotlin.annotation.AnnotationTarget.*
+
+/**
+ * Annotate the etcd version that the function, field or type works with.
+ *
+ * create at 2017/4/10
+ * @author liufl
+ * @since 0.1.0
+ */
+@MustBeDocumented
+@Retention(BINARY)
+@Target(CLASS, FUNCTION, FIELD)
+annotation class ForEtcdVersion(val since: EtcdVersion, val until: EtcdVersion = EtcdVersion.V3_1_5)
