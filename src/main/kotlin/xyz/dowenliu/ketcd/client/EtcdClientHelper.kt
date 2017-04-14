6d235494b8917f1c3a43016bcc79f8cb27cb079f
dowenliu-xyz <hawkdowen@hotmail.com>
2017-04-14 10:59:02 +0800

    change: util file EtcdClientHelper
    add fun configureStub()

--- src/main/kotlin/xyz/dowenliu/ketcd/client/EtcdClientHelper.kt
+++ src/main/kotlin/xyz/dowenliu/ketcd/client/EtcdClientHelper.kt
@@ -4,13 +4,14 @@ package xyz.dowenliu.ketcd.client
 
 import io.grpc.ManagedChannelBuilder
 import io.grpc.NameResolver
+import io.grpc.stub.AbstractStub
 import xyz.dowenliu.ketcd.Endpoint
 import xyz.dowenliu.ketcd.resolver.SimpleEtcdNameResolverFactory
 import java.net.URI
 import java.util.stream.Collectors
 
 /**
- * Help build etcd clients
+ * Help build etcd client
  *
  * create at 2017/4/13
  * @author liufl
@@ -22,3 +23,10 @@ internal fun defaultChannelBuilder(factory: NameResolver.Factory): ManagedChanne
 
 internal fun simpleNameResolverFactory(endpoints: List<Endpoint>): NameResolver.Factory =
         SimpleEtcdNameResolverFactory(endpoints.stream().map { URI(it.toString()) }.collect(Collectors.toList()))
+
+internal fun <T: AbstractStub<T>> configureStub(stub: T, token: String?): T {
+    token ?: return stub
+    val metadata = io.grpc.Metadata()
+    metadata.put(io.grpc.Metadata.Key.of("token", io.grpc.Metadata.ASCII_STRING_MARSHALLER), token)
+    return stub.withCallCredentials { _, _, _, applier -> applier.apply(metadata) }
+}
