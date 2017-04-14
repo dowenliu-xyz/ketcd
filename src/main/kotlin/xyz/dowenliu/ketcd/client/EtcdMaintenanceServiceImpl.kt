d202748ae31c91712ea4ba3bfe4a315cfe1bd151
dowenliu-xyz <hawkdowen@hotmail.com>
2017-04-14 13:49:09 +0800

    change: class EtcdMaintenanceServiceImpl
    implements fun deactiveAlarm

--- src/main/kotlin/xyz/dowenliu/ketcd/client/EtcdMaintenanceServiceImpl.kt
+++ src/main/kotlin/xyz/dowenliu/ketcd/client/EtcdMaintenanceServiceImpl.kt
@@ -2,10 +2,7 @@ package xyz.dowenliu.ketcd.client
 
 import io.grpc.ManagedChannel
 import io.grpc.stub.StreamObserver
-import xyz.dowenliu.ketcd.api.AlarmRequest
-import xyz.dowenliu.ketcd.api.AlarmResponse
-import xyz.dowenliu.ketcd.api.AlarmType
-import xyz.dowenliu.ketcd.api.MaintenanceGrpc
+import xyz.dowenliu.ketcd.api.*
 import java.io.Closeable
 
 /**
@@ -32,8 +29,34 @@ class EtcdMaintenanceServiceImpl internal constructor(val channel: ManagedChanne
 
     override fun listAlarms(): AlarmResponse = blockingStub.alarm(listAlarmsRequest())
 
-    override fun listAlarmsAsync(callback: EtcdMaintenanceService.AlarmListCallback) {
-        asyncStub.alarm(listAlarmsRequest(), object : StreamObserver<AlarmResponse> {
+    override fun listAlarmsAsync(callback: EtcdMaintenanceService.AlarmListCallback) =
+            asyncStub.alarm(listAlarmsRequest(), object : StreamObserver<AlarmResponse> {
+                override fun onNext(value: AlarmResponse?) {
+                    value?.let { callback.onResponse(it) }
+                }
+
+                override fun onError(t: Throwable?) {
+                    t?.let { callback.onError(it) }
+                }
+
+                override fun onCompleted() = callback.completeCallback()
+            })
+
+    private fun deactiveAlarmRequest(member: AlarmMember): AlarmRequest {
+        val memberID = member.memberID
+        require(memberID != 0L, { "The member id can not be 0." })
+        require(member.alarm != AlarmType.NONE, { "Alarm type is NONE." })
+        return AlarmRequest.newBuilder()
+                .setAlarm(AlarmType.NOSPACE)
+                .setAction(AlarmRequest.AlarmAction.DEACTIVATE)
+                .setMemberID(memberID)
+                .build()
+    }
+
+    override fun deactiveAlarm(member: AlarmMember): AlarmResponse = blockingStub.alarm(deactiveAlarmRequest(member))
+
+    override fun deactiveAlarm(member: AlarmMember, callback: EtcdMaintenanceService.DeactiveAlarmCallback) {
+        asyncStub.alarm(deactiveAlarmRequest(member), object : StreamObserver<AlarmResponse> {
             override fun onNext(value: AlarmResponse?) {
                 value?.let { callback.onResponse(it) }
             }
