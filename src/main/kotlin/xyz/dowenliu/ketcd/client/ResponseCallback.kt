893ae450fd4f9403325ab64499eb7bac908e850f
dowenliu-xyz <hawkdowen@hotmail.com>
2017-04-14 14:26:15 +0800

    add: interface ResponseCallback
    Handles received response.

---
+++ src/main/kotlin/xyz/dowenliu/ketcd/client/ResponseCallback.kt
@@ -0,0 +1,27 @@
+package xyz.dowenliu.ketcd.client
+
+/**
+ * Callback handling received response.
+ *
+ * create at 2017/4/14
+ * @author liufl
+ * @since 0.1.0
+ */
+interface ResponseCallback<T> {
+    /**
+     * Handle response received.
+     *
+     * @param response The response received.
+     */
+    fun onResponse(response: T)
+
+    /**
+     * When exception caught
+     */
+    fun onError(throwable: Throwable)
+
+    /**
+     * To complete this callback
+     */
+    fun completeCallback()
+}
