28395e8b4f7d0eb58c31eeacf2119387816dc74e
dowenliu-xyz <hawkdowen@hotmail.com>
2017-04-14 14:17:52 +0800

    change: class EtcdClient
    Don't use RoundRobinLoadBalanceFactory by default. Use Builder's withCannelBuilder() fun to set a ManagedChannelBuilder with a specific LoadBalanceFactory instead.

--- src/main/kotlin/xyz/dowenliu/ketcd/client/EtcdClient.kt
+++ src/main/kotlin/xyz/dowenliu/ketcd/client/EtcdClient.kt
@@ -2,7 +2,6 @@ package xyz.dowenliu.ketcd.client
 
 import io.grpc.ManagedChannel
 import io.grpc.ManagedChannelBuilder
-import io.grpc.util.RoundRobinLoadBalancerFactory
 import xyz.dowenliu.ketcd.Endpoint
 import xyz.dowenliu.ketcd.UsernamePassword
 import xyz.dowenliu.ketcd.api.AuthGrpc
@@ -51,12 +50,10 @@ class EtcdClient(val channelBuilder: ManagedChannelBuilder<*>,
                                 .setPasswordBytes(usernamePassword.password)
                                 .build()
                 )
-
-        private val rbLoadBalanceFactory = RoundRobinLoadBalancerFactory.getInstance()
     }
 
     fun newMaintenanceService(): EtcdMaintenanceService =
-            EtcdMaintenanceServiceImpl(channelBuilder.loadBalancerFactory(rbLoadBalanceFactory).build(), token)
+            EtcdMaintenanceServiceImpl(channelBuilder.build(), token)
 
     class Builder internal constructor() {
         private val _endpoints: MutableList<Endpoint> = mutableListOf()
