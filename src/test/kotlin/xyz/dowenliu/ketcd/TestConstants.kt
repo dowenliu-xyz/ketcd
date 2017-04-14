b4127ccbf48e856eedfa55521d3f598bd5a98229
dowenliu-xyz <hawkdowen@hotmail.com>
2017-04-14 10:39:38 +0800

    add: test util TestConstants
    Etcd endpoints and peerUrls.

---
+++ src/test/kotlin/xyz/dowenliu/ketcd/TestConstants.kt
@@ -0,0 +1,15 @@
+@file:JvmName("TestConstants")
+
+package xyz.dowenliu.ketcd
+
+/**
+ * Test constants, contain the cluster info
+ *
+ * create at 2017/4/9
+ * @author liufl
+ * @since 0.1.0
+ */
+
+val endpoints = arrayOf("http://localhost:2379", "http://localhost:22379", "http://localhost:32379")
+
+val peerUrls = arrayOf("http://localhost:12380", "http://localhost:22380", "http://localhost:32380")
