b0dc131396971622adb97a52a08d883a1099da94
dowenliu-xyz <hawkdowen@hotmail.com>
2017-04-17 09:21:42 +0800

    add: util file KeyHelper
    Provide or product keys for kv operation option

---
+++ src/main/kotlin/xyz/dowenliu/ketcd/kv/KeyHelper.kt
@@ -0,0 +1,34 @@
+@file:JvmName("KeyHelper")
+
+package xyz.dowenliu.ketcd.kv
+
+import com.google.protobuf.ByteString
+import xyz.dowenliu.ketcd.protobuf.oneBitLarger
+import xyz.dowenliu.ketcd.protobuf.toByteString
+
+/**
+ * create at 2017/4/17
+ * @author liufl
+ * @since 0.1.0
+ */
+
+/**
+ * EndKey option value for get/delete range all keys >= key (--from-key).
+ */
+val FROM_KEY: ByteString = ByteString.copyFrom("\u0000", Charsets.UTF_8)
+
+val NULL_KEY: ByteString = ByteString.copyFrom(byteArrayOf('\u0000'.toByte()))
+
+/**
+ * EndKey option value for get/delete prefixed by [string] (--prefix)
+ *
+ * @param string The prefix string.
+ */
+fun prefixKeyOf(string: ByteString): ByteString = string.oneBitLarger()
+
+/**
+ * EndKey option value for get/delete prefixed by [string] (--prefix)
+ *
+ * @param string The prefix string.
+ */
+fun prefixKeyOf(string: String): ByteString = prefixKeyOf(string.toByteString())
