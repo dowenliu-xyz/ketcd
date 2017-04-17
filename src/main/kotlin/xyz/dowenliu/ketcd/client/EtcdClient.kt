51b5f98ab552844c7e523f6349e17b5e77992c3b
dowenliu-xyz <hawkdowen@hotmail.com>
2017-04-17 14:42:51 +0800

    change: class EtcdClient
    add fun newAuthService

--- src/main/kotlin/xyz/dowenliu/ketcd/client/EtcdClient.kt
+++ src/main/kotlin/xyz/dowenliu/ketcd/client/EtcdClient.kt
@@ -96,6 +96,8 @@ class EtcdClient(val channelBuilder: ManagedChannelBuilder<*>,
 
     fun newKVService(): EtcdKVService = EtcdKVServiceImpl(channelBuilder.build(), token)
 
+    fun newAuthService(): EtcdAuthService = EtcdAuthServiceImpl(channelBuilder.build(), token)
+
     class Builder internal constructor() {
         private val _endpoints: MutableList<Endpoint> = mutableListOf()
         /**
