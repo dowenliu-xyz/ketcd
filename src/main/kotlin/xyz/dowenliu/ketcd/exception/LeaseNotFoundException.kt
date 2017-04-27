4aceb8a9d2f5f9569695d9edb32f427168f539e7
dowenliu-xyz <hawkdowen@hotmail.com>
2017-04-27 11:21:39 +0800

    add: exception LeaseNotFoundException

---
+++ src/main/kotlin/xyz/dowenliu/ketcd/exception/LeaseNotFoundException.kt
@@ -0,0 +1,8 @@
+package xyz.dowenliu.ketcd.exception
+
+/**
+ * create at 2017/4/26
+ * @author liufl
+ * @since 0.1.0
+ */
+class LeaseNotFoundException(message: String? = null, cause: Throwable? = null): Exception(message, cause)
