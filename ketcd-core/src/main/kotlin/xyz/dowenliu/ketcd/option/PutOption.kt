a58f678b30d5f145060e959189506fd73b085acf
dowenliu-xyz <hawkdowen@hotmail.com>
2017-06-13 14:49:43 +0800

    changer: PutOption
    add options ignore-value, ignore-lease

--- ketcd-core/src/main/kotlin/xyz/dowenliu/ketcd/option/PutOption.kt
+++ ketcd-core/src/main/kotlin/xyz/dowenliu/ketcd/option/PutOption.kt
@@ -12,11 +12,13 @@ import xyz.dowenliu.ketcd.version.ForEtcdVersion
  *
  * @property leaseId The lease id bind to.
  * @property prevKV If the response will contains previous key-value pair.
+ * @property ignoreValue updates the key using its current value
+ * @property ignoreLease updates the key using its current lease
  */
-// TODO ignore-value
-// TODO ignore-lease
 class PutOption private constructor(val leaseId: Long,
-                                    @ForEtcdVersion(EtcdVersion.V3_0_11) val prevKV: Boolean) {
+                                    @ForEtcdVersion(EtcdVersion.V3_0_11) val prevKV: Boolean,
+                                    @ForEtcdVersion(EtcdVersion.V3_2_0_rc0) val ignoreValue: Boolean,
+                                    @ForEtcdVersion(EtcdVersion.V3_2_0_rc0) val ignoreLease: Boolean) {
     /**
      * Companion object of [PutOption]
      */
@@ -40,6 +42,8 @@ class PutOption private constructor(val leaseId: Long,
     class Builder internal  constructor() {
         private var leaseId = 0L
         private var prevKV = false
+        private var ignoreValue = false
+        private var ignoreLease = false
 
         /**
          * Assign a _leaseId_ for a put operation. Zero means no lease.
@@ -67,10 +71,34 @@ class PutOption private constructor(val leaseId: Long,
         }
 
         /**
+         * Updates the key using its current value (ignore given value)
+         *
+         * @param ignoreValue if updates the key using its current value.
+         * @return this builder to train.
+         */
+        @ForEtcdVersion(EtcdVersion.V3_2_0_rc0)
+        fun ignoreValue(ignoreValue: Boolean = true): Builder {
+            this.ignoreValue = ignoreValue
+            return this
+        }
+
+        /**
+         * Updates the key using its current lease
+         *
+         * @param ignoreLease if updates the key using its current lease.
+         * @return this builder to train.
+         */
+        @ForEtcdVersion(EtcdVersion.V3_2_0_rc0)
+        fun ignoreLease(ignoreLease: Boolean = true): Builder {
+            this.ignoreLease = ignoreLease
+            return this
+        }
+
+        /**
          * Build the put option
          *
          * @return the put option
          */
-        fun build(): PutOption = PutOption(leaseId, prevKV)
+        fun build(): PutOption = PutOption(leaseId, prevKV, ignoreValue, ignoreLease)
     }
 }
