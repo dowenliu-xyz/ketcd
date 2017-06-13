4cf693396a787a20337e9eafda5955606a894249
dowenliu-xyz <hawkdowen@hotmail.com>
2017-06-13 09:02:44 +0800

    change: enum class EtcdVersion and annotation class ForEtcdVersion
    add two release: v3.1.9 and v3.2.0

--- ketcd-core/src/main/kotlin/xyz/dowenliu/ketcd/version/ForEtcdVersion.kt
+++ ketcd-core/src/main/kotlin/xyz/dowenliu/ketcd/version/ForEtcdVersion.kt
@@ -16,4 +16,4 @@ import kotlin.annotation.AnnotationTarget.*
 @MustBeDocumented
 @Retention(BINARY)
 @Target(CLASS, FUNCTION, FIELD)
-annotation class ForEtcdVersion(val since: EtcdVersion, val until: EtcdVersion = EtcdVersion.V3_2_0_rc0)
+annotation class ForEtcdVersion(val since: EtcdVersion, val until: EtcdVersion = EtcdVersion.V3_2_0)
